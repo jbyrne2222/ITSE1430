8e9acd6fc63ae49d42082160a6f14630c1b9ac54
jonathan.byrne707 <jonathan.byrne707@my.tccd.edu>
2018-01-24 19:35:01 -0600

    HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    // Single line comment
+    class Program
+    {
+        /*
+         * As many lines as you want
+         */
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
