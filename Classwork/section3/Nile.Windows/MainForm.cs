74d85a1710633d8371f20fdc26fcc4054c6b8eab
Jonathan Byrne <35551697+jbyrne2222@users.noreply.github.com>
2018-02-26 21:48:19 -0600

    Add files via upload

---
+++ Classwork/section3/Nile.Windows/MainForm.cs
@@ -0,0 +1,159 @@
+﻿/*
+ * Jonathan Byrne
+ * ITSE 1430
+ * Section 3 classwork
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nile.Data.Memory;
+
+namespace Nile.Windows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+            RefreshUI();
+        }
+
+        private void PlayingWithProductMembers ()
+        { 
+            var product = new Product();
+
+            Decimal.TryParse("123", out var price);
+            product.Price = price;
+
+            var name = product.Name;
+            //var name = product.GetName();
+            product.Name = "Product A";
+            product.Price = 50;
+            product.IsDiscontinued = true;
+
+            //product.ActualPrice = 10;
+            var price2 = product.ActualPrice;
+
+            //product.SetName("Product A");
+            //product.Description = "None";
+            var error = product.Validate();
+
+            var str = product.ToString();
+
+            var productB = new Product();
+            //productB.Name = "Product B";
+            //productB.SetName("Product B");
+            //productB.Description = product.Description;
+            error = productB.Validate();
+
+        }
+
+        #region Event Handlers
+
+        private void OnProductAdd ( object sender, EventArgs e )
+        {
+            var button = sender as ToolStripMenuItem;
+
+            var form = new ProductDetailForm("Add Product");          
+
+            //Show form modally
+            var result = form.ShowDialog(this);
+            if (result != DialogResult.OK)
+                return;
+
+            //Add to database
+            _database.Add(form.Product, out var message);
+            if (!String.IsNullOrEmpty(message))
+                MessageBox.Show(message);
+        }
+
+        private void OnProductEdit( object sender, EventArgs e )
+        {
+            // Get the first product
+            var products = _database.GetAll();
+            var product = (products.Length > 0) ? products[0] : null;
+            if (product == null)
+                return;
+
+            //var index = FindEmptyProductIndex() - 1;
+            //if (index < 0)
+            //    return;
+            //if (_product == null)
+            //    return;
+
+            var form = new ProductDetailForm(product);
+            var result = form.ShowDialog(this);
+            if (result != DialogResult.OK)
+                return;
+
+            //"Editing" the product
+            _database.Edit(form.Product, out var message);
+            if (!String.IsNullOrEmpty(message))
+                MessageBox.Show(message);
+        }
+
+        private void OnProductRemove( object sender, EventArgs e )
+        {
+            // Get the first product
+            var products = _database.GetAll();
+            var product = (products.Length > 0) ? products[0] : null;
+            if (product == null)
+                return;
+
+            //var index = FindEmptyProductIndex() - 1;
+            //if (index < 0)
+            //   return;
+
+            if (!ShowConfirmation("Are you sure?", "Remove Product"))
+                return;
+
+
+            //Remove product
+            _database.Remove(product.Id);
+            //_products[index] = null;
+
+        }
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            Close();
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            MessageBox.Show(this, "Not implemented", "Help About", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+        #endregion
+
+        private void RefreshUI ()
+        {
+            //Get products
+            var products = _database.GetAll();
+
+            //Bind to grid
+            dataGridView1.DataSource = products;
+        }
+
+        private bool ShowConfirmation ( string message, string title )
+        {
+            return MessageBox.Show(this, message, title,
+               MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+               == DialogResult.Yes;
+        }
+
+        private MemoryProductDatabase _database = new MemoryProductDatabase();
+    }
+}
---
+++ Classwork/section3/Nile/Product.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Jonathan Byrne
+ * ITSE 1430
+ * Section 2 classwork
+ */
+
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile
+{
+    /// <summary>Provides information about a product.</summary>
+    public class Product
+    {
+        /// <summary>Gets or sets the product ID.</summary>
+        public int Id { get; set; }
+
+        internal decimal DiscountPercentage = 0.10M;
+
+        /// <summary>Gets or sets the name.</summary>
+        /// <value></value>
+        public string Name
+        {
+            get { return _name ?? ""; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description ?? ""; }
+            set { _description = value?? ""; }
+        }
+
+        //Using auto property here
+        public decimal Price
+        {
+            //get { return _price; }
+            //set { _price = value; }
+            get; set;
+        } = 0;
+
+        //public int ShowingOffAcessibility
+        //{
+        //    get { }
+        //    internal set { }
+        //}
+
+        public decimal ActualPrice
+        {
+            get 
+            {
+                if (IsDiscontinued)
+                    return Price - (Price * DiscountPercentage);
+
+                return Price;
+            }
+
+            //set { }
+        }
+
+        public bool IsDiscontinued { get; set; }
+        //{
+        //    get { return _isDiscontinued; }
+        //    set { _isDiscontinued = value; }
+        //}
+
+        /// <summary>Get the product name.</summary>
+        /// <returns>The name.</returns>
+        //public string GetName ()
+        //{
+        //    return _name ?? "";
+        //}
+
+        /// <summary>Sets the product name.</summary>
+        /// <param name="value">The name.</param>
+        //public void SetName (string value)
+        //{
+        //    _name = value ?? "";
+        //}
+
+        /// <summary>Validates the product.</summary>
+        /// <returns>Error message, if any.</returns>
+        public string Validate ()
+        {
+            // Name is required
+            if (String.IsNullOrEmpty(_name))
+                return "Name cannot be empty";
+
+            //Price >= 0
+            if (Price < 0)
+                return "Price must be >= 0";
+
+            return "";
+        }
+
+        private string _name;
+        private string _description;
+        //private decimal _price;
+        //private bool _isDiscontinued;
+    }
+}
