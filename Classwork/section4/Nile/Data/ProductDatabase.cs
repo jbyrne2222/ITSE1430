decfe6055404ab3236afdc067228cf6f80103881
jonathan.byrne707 <jonathan.byrne707@my.tccd.edu>
2018-03-26 21:36:25 -0500

    Section 4

---
+++ Classwork/section4/Nile/Data/ProductDatabase.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Data
+{
+    /// <summary>Provides an in-memory product database.</summary>
+    public abstract class ProductDatabase : IProductDatabase
+    {
+
+        public Product Add ( Product product, out string message )
+        {
+            if (product == null)
+            {
+                message = "Product cannot be null.";
+                return null;
+            };
+
+            //Validate product
+            var errors = product.Validate();
+            //var errors = ObjectValidator.Validate(product);
+
+            //if(errors.Count() > 0)
+            //{
+            //    var error = Enumerable.First(errors);
+
+            //    //Get first error
+            //    message = errors.ElementAt(0).ErrorMessage;
+            //    return null;
+            //};
+
+            var error = errors.FirstOrDefault();
+            if (error != null)
+            {
+                message = error.ErrorMessage;
+                return null;
+            };
+
+            // Verify unique product
+            var existing = GetProductByNameCore(product.Name);
+            if(existing != null)
+            {
+                message = "Product already exists";
+                return null;
+            };
+
+            message = null;
+            return AddCore(product);
+        }
+
+        public Product Update( Product product, out string message )
+        {
+            message = "";
+
+            //Check for null
+            if (product == null)
+            {
+                message = "Product cannot be null.";
+                return null;
+            };
+
+            //Validate product using IValidateableObject
+            var errors = ObjectValidator.Validate(product);
+            if (errors.Count() > 0)
+            {
+                message = errors.ElementAt(0).ErrorMessage;
+                return null;
+            };
+
+            // Verify unique product
+            var existing = GetProductByNameCore(product.Name);
+            if (existing != null && existing.Id != product.Id)
+            {
+                message = "Product already exists";
+                return null;
+            }
+
+            //Find existing
+            existing = existing ?? GetCore(product.Id);
+            if (existing== null)
+            {
+                message = "Product not found.";
+                return null;
+            };
+
+            return UpdateCore(product);
+        }
+
+        /// <summary>Gets all the products.</summary>
+        /// <returns>The list of products.</returns>
+        public IEnumerable<Product> GetAll()
+        {
+            return GetAllCore();
+        }
+
+        //public IEnumerable<Product> GetAll ()
+        //{
+        //    //Return a copy so caller cannot change the underlying data
+        //    var items = new List<Product>();
+        //
+        //    //for (var index = 0; index < _products.Length; ++index)
+        //    foreach ( var product in _products)
+        //    {
+        //        if (product != null)
+        //            items.Add(Clone(product));
+        //    };
+        //
+        //    return items;
+        //}
+        //
+
+        /// <summary>Removes a product. </summary>
+        /// <param name="id">The product ID.</param>
+        public void Remove (int id)
+        {
+            if (id > 0)
+            {
+                RemoveCore(id);
+            };
+        }
+
+        protected abstract Product AddCore( Product product );
+        protected abstract IEnumerable<Product> GetAllCore();
+        protected abstract Product GetCore( int id );
+        protected abstract Product UpdateCore( Product product );
+        protected abstract void RemoveCore( int id );
+        protected abstract Product GetProductByNameCore( string name );
+    }
+}
---
+++ Classwork/section4/Nile/Product.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Jonathan Byrne
+ * ITSE 1430
+ * Section 2 classwork
+ */
+
+ using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile
+{
+    /// <summary>Provides information about a product.</summary>
+    public class Product : IValidatableObject
+    {
+        /// <summary>Gets or sets the product ID.</summary>
+        public int Id { get; set; }
+
+        internal decimal DiscountPercentage = 0.10M;
+
+        /// <summary>Gets or sets the name.</summary>
+        /// <value></value>
+        public string Name
+        {
+            get { return _name ?? ""; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description ?? ""; }
+            set { _description = value?? ""; }
+        }
+
+        //Using auto property here
+        public decimal Price
+        {
+            //get { return _price; }
+            //set { _price = value; }
+            get; set;
+        } = 0;
+
+        //public int ShowingOffAcessibility
+        //{
+        //    get { }
+        //    internal set { }
+        //}
+
+        public decimal ActualPrice
+        {
+            get 
+            {
+                if (IsDiscontinued)
+                    return Price - (Price * DiscountPercentage);
+
+                return Price;
+            }
+
+            //set { }
+        }
+
+        public bool IsDiscontinued { get; set; }
+        //{
+        //    get { return _isDiscontinued; }
+        //    set { _isDiscontinued = value; }
+        //}
+
+        /// <summary>Get the product name.</summary>
+        /// <returns>The name.</returns>
+        //public string GetName ()
+        //{
+        //    return _name ?? "";
+        //}
+
+        /// <summary>Sets the product name.</summary>
+        /// <param name="value">The name.</param>
+        //public void SetName (string value)
+        //{
+        //    _name = value ?? "";
+        //}
+
+        /// <summary>Validates the product.</summary>
+        /// <param name="validationContext">The validation context.</param>
+        /// <returns>The validation results.</returns>
+        public IEnumerable<ValidationResult> Validate( ValidationContext validationContext )
+        {
+            var errors = new List<ValidationResult>();
+
+            // Name is required
+            if (String.IsNullOrEmpty(_name))
+                errors.Add(new ValidationResult("Name cannot be empty",
+                             new[] { nameof(Name) }));
+
+            //Price >= 0
+            if (Price < 0)
+                errors.Add(new ValidationResult("Price must be >= 0",
+                             new[] { nameof(Price) }));
+
+            return errors;
+        }
+
+        private string _name;
+        private string _description;
+        //private decimal _price;
+        //private bool _isDiscontinued;
+    }
+}
